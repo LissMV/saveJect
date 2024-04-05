19fb41f73ecf65b85f0233e438d5d269ed7092d6
Lisseth Mejía <valencialisseth47@gmail.com>
2024-04-05 12:03:37 -0600

    login_style

--- resources/views/components/input.blade.php
+++ resources/views/components/input.blade.php
@@ -1,7 +1,6 @@
 @props(['titulo', 'tipo', 'rex'])
 <div class="my-4">
-  <label class="pr-6">{{ $titulo }} <label>
-  <input name={{ $rex }} type={{ $tipo }} class=" border-b-4 text-orange-600 rounded-full px-9 py-2 placeholder={{$titulo}}">
+  <input name={{ $rex }} type={{ $tipo }} class="border-b-4 text-orange-600 rounded-full px-9 py-2" placeholder={{$titulo}}>
   @error($rex)
   <p class="text-blue-500">{{$message}}</p>
   @enderror
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,40 +1,42 @@
 <x-layout>
-   <body>
-      <style>
 
-        body {
-            background: url('https://i.pinimg.com/564x/94/57/53/94575326e5fbec9d470d1d4ff2dc3e27.jpg');
-            background-size: cover;
-            background-attachment: fixed;
-            background-position: center;
-         }
+    <body>
+        <style>
+            body {
+                background: url('https://i.pinimg.com/564x/94/57/53/94575326e5fbec9d470d1d4ff2dc3e27.jpg');
+                background-size: cover;
+                background-attachment: fixed;
+                background-position: center;
+            }
 
-         #glassEffect {
-            background: linear-gradient(135deg, rgba(225, 225, 225, 0.1), rgba(225, 225, 225, 0));
-            backdrop-filter: blur(10px);
-            -webkit-backdrop-filter: blur(10px);
-            border-radius: 20px;
-            border: 1px solid rgba(225, 225, 225, 0.18);
-            box-shadow: 0 8px 32px 0 rgba(0, 0, 0, 0, 37);
-         }
-      </style>
-   </body>
-   <div id="glassEffect" class="mt-6 w-full max-w-md rounded-lg p-8">
+            #glassEffect {
+                background: linear-gradient(135deg, rgba(225, 225, 225, 0.1), rgba(225, 225, 225, 0));
+                backdrop-filter: blur(10px);
+                -webkit-backdrop-filter: blur(10px);
+                border-radius: 20px;
+                border: 1px solid rgba(225, 225, 225, 0.18);
+                box-shadow: 0 8px 32px 0 rgba(0, 0, 0, 0, 37);
+            }
+        </style>
+    </body>
 
-     <h1 class="font-black text-4xl text-center">Sign In</h1>
-     <form action="/login" method="POST">
-        @csrf
-      <div>
-        <x-input rex="email" titulo="Email" tipo="text"/>
-      </div>
-      <div>
-        <x-input rex="password" titulo="Password" tipo="password"/>
-      </div>
-      <div>
-      <button  class="w-full bg-emerald-500 text-white font-semibold py-2 px-4 rounded-full hover:bg-emerald-600 focus:outline-none focus:bg-emerald-600" type="submit">Log In</button>
-      </div>
-     </form>
-
-  </div>
- </div>
+    <div class="flex justify-center items-center h-screen">
+        <div id="glassEffect" class="w-full max-w-md rounded-lg p-8">
+            <h1 class="font-black text-4xl text-center">Sign In</h1>
+            <form action="/login" method="POST" class="text-center">
+                @csrf
+                <div>
+                    <x-input rex="email" titulo="Email" tipo="text" />
+                </div>
+                <div>
+                    <x-input rex="password" titulo="Password" tipo="password" />
+                </div>
+                <div>
+                    <button
+                        class="w-full bg-emerald-500 text-white font-semibold py-2 px-4 rounded-full hover:bg-emerald-600 focus:outline-none focus:bg-emerald-600"
+                        type="submit">Log In</button>
+                </div>
+            </form>
+        </div>
+    </div>
 </x-layout>
